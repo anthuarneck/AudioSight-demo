96534b4066b13ed03f306e41ef3d41e3bf7c8a47
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-09-16 14:08:34 -0400

    Edits.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import "./App.css";
 import { getToken } from "../fetch";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Success from "./components/Success";
 import LoginButton from "./components/LoginButton";
 
-function getArtist() {
-  return fetch("https://api.spotify.com/v1/artists/3wcj11K77LjEY1PkEazffa", {
-    headers: {
-      Authorization:
-        "Bearer BQCqhgO6vlbuXBW5AbZ-1Ix6fVeBMVZ36M85dpqEtU23EHojulOmHTaqRnfdOqVVR83Yt0dboMxjw4c6EnDgZb0HAybgHDigVXbQo8cWSHtXUNaBzdI", //Access token
-    },
-  }).then((res) => res.json());
-}
 
 var client_id = "8b34a109eb1244189620da8eba0cafd8";
 var redirect_uri = "http://localhost:5173/success";
 
-console.log(getArtist())
-
-
 function App() {
+
+const [artistData, setArtistData] = useState(null)
+
+useEffect(() => {
+  fetch("https://api.spotify.com/v1/artists/3wcj11K77LjEY1PkEazffa", {
+    headers: {
+      Authorization:
+        "Bearer BQDzvD6K5hbDUIRR047LXZoMFEbp5Y-VeNe34b_ytkzX8Xgj3PwT1onkhOlfkVxDnslZQOFyVTph9KWWS9FM3tv5SLtMKl_gZx3Y92BsStLsKgwU588", //Access token
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      setArtistData(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}, [])
+
+
   return (
     <Router>
       <Routes>
         {/* Define your routes here */}
         <Route path="/" element={<Home />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/success" element={<Success artistData={artistData}/>} />
       </Routes>
     </Router>
   );
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Success = () => {
+
+const Success = ({artistData}) => {
+
+    console.log(artistData)
+
   return (
     <div>
       <h1>Success Works!</h1>
+      {/* <p>{artistName}</p> */}
     </div>
   );
 };
