94d8f58547a799b8754a7f70fcaf7ae62c575e4b
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-09-17 15:03:22 -0400

    testing success page.

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,32 +4,32 @@ import Search from "./Search";
 
 const Success = ({ token, setToken }) => {
 
-  const [artistData, setArtistData] = useState(null);
+  // const [artistData, setArtistData] = useState(null);
 
-  console.log(token) 
+  // console.log(token) 
 
-  useEffect(() => {
-    fetch("https://api.spotify.com/v1/artists/3wcj11K77LjEY1PkEazffa", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setArtistData(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://api.spotify.com/v1/artists/3wcj11K77LjEY1PkEazffa", {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setArtistData(data);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // }, []);
 
-  if (!artistData || !artistData.name) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
+  // if (!artistData || !artistData.name) {
+  //   return (
+  //     <div>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
