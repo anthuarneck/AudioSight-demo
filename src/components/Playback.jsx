35c0acd9a046be606ebd3033ee8219541f8e948f
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-09-18 22:24:30 -0400

    edits.

--- src/components/Playback.jsx
+++ src/components/Playback.jsx
@@ -1,64 +1,64 @@
 import React, { useEffect, useState } from "react";
 
 const Playback = ({ token }) => {
+  const [playerInstance, setPlayerInstance] = useState(null);
 
-    const [playerInstance, setPlayerInstance] = useState(null);
+  useEffect(() => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new Spotify.Player({
+        name: "Web Playback SDK Quick Start Player",
+        getOAuthToken: (cb) => {
+          cb(token);
+        },
+      });
 
-    useEffect(() => {
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            const player = new Spotify.Player({
-                name: "Web Playback SDK Quick Start Player",
-                getOAuthToken: (cb) => {
-                    cb(token);
-                },
-            });
+      player.addListener("initialization_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("authentication_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("account_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("playback_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("player_state_changed", (state) => {
+        console.log(state);
+      });
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
 
-            player.addListener("initialization_error", ({ message }) => {
-                console.error(message);
-            });
-            player.addListener("authentication_error", ({ message }) => {
-                console.error(message);
-            });
-            player.addListener("account_error", ({ message }) => {
-                console.error(message);
-            });
-            player.addListener("playback_error", ({ message }) => {
-                console.error(message);
-            });
-            player.addListener("player_state_changed", (state) => {
-                console.log(state);
-            });
-            player.addListener("ready", ({ device_id }) => {
-                console.log("Ready with Device ID", device_id);
-            });
+      player.connect();
 
-            player.connect();
-
-            setPlayerInstance(player);
-        };
-
-        const script = document.createElement("script");
-        script.src = "https://sdk.scdn.co/spotify-player.js";
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, [token]);
-
-    const togglePlayback = () => {
-        if (playerInstance) {
-            playerInstance.togglePlay();
-        }
+      setPlayerInstance(player);
     };
 
-    return (
-        <div>
-            <button id="togglePlay" onClick={togglePlayback}>
-                Toggle Play
-            </button>
-        </div>
-    );
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    document.body.appendChild(script);
+
+    return () => {
+      delete window.onSpotifyWebPlaybackSDKReady;
+      document.body.removeChild(script);
+    };
+  }, [token]);
+
+  const togglePlayback = () => {
+    if (playerInstance) {
+      playerInstance.togglePlay();
+    }
+  };
+
+  return (
+    <div>
+      <button id="togglePlay" onClick={togglePlayback}>
+        Toggle Play
+      </button>
+    </div>
+  );
 };
 
 export default Playback;
