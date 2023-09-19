2f72e8cfe42b0704a1da2ec7464173b83bb307fa
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-09-18 22:40:25 -0400

    Adds to playback functionality.

--- src/components/Playback.jsx
+++ src/components/Playback.jsx
@@ -52,25 +52,65 @@ const Playback = ({ token, selectedTrack }) => {
     }
   };
 
+  const id = selectedTrack.id;
+  const playSpecificTrack = (id) => {
+    if (playerInstance) {
+      playerInstance._options.getOAuthToken((token) => {
+        fetch(
+          "https://api.spotify.com/v1/me/player/play?device_id=" +
+            playerInstance._options.id,
+          {
+            method: "PUT",
+            body: JSON.stringify({ uris: [id] }),
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      });
+    }
+  };
+
+  const skipToNext = () => {
+    if (playerInstance) {
+      playerInstance.nextTrack().then(() => {
+        console.log("Skipped to next track!");
+      });
+    }
+  };
+
+  const skipToPrevious = () => {
+    if (playerInstance) {
+      playerInstance.previousTrack().then(() => {
+        console.log("Skipped to previous track!");
+      });
+    }
+  };
+
+  const pausePlayback = () => {
+    if (playerInstance) {
+      playerInstance.pause().then(() => {
+        console.log("Paused playback!");
+      });
+    }
+  };
+
   return (
     <div>
-        <button id="togglePlay" onClick={togglePlayback}>
-            Toggle Play
-        </button>
-        <button onClick={() => playSpecificTrack(`spotify:track:${selectedTrack.id}`)}>
-            Play Specific Track
-        </button>
-        <button onClick={skipToNext}>
-            Skip to Next
-        </button>
-        <button onClick={skipToPrevious}>
-            Skip to Previous
-        </button>
-        <button onClick={pausePlayback}>
-            Pause
-        </button>
+      <button id="togglePlay" onClick={togglePlayback}>
+        Toggle Play
+      </button>
+      <button
+        onClick={() => playSpecificTrack(`spotify:track:${selectedTrack.id}`)}
+      >
+        Play Specific Track
+      </button>
+      <button onClick={skipToNext}>Skip to Next</button>
+      <button onClick={skipToPrevious}>Skip to Previous</button>
+      <button onClick={pausePlayback}>Pause</button>
     </div>
-);
+  );
 };
 
 export default Playback;
